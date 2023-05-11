9d846db8942e8f948bcf368f257565908d93ce13
sexomq <sclifos-tudor@mail.ru>
2023-05-12 00:46:40 +0300

    changes in Parser

--- ELSD/ParserClass.py
+++ ELSD/ParserClass.py
@@ -1,18 +1,29 @@
+#       Made with <3
+########################################
+#       This is still in the
+#       development stage.
+#       Please, be patient
+#       for a better structure.
+#########################################
+
 import pandas as pd
-from pyparsing import CaselessKeyword, Word, alphas, nums, Optional, Combine, Group, delimitedList, ParseException
+from pyparsing import CaselessKeyword, Word, alphas, nums, Optional, Combine, Group, delimitedList, ParseException, \
+    QuotedString, Suppress, oneOf
 
 # Define the SQL syntax
-select_stmt = CaselessKeyword('select') + (Word('*') | delimitedList(Word(alphas), ','))('columns') + \
+select_stmt = CaselessKeyword('select') + delimitedList(Word(alphas), ',')('columns') + \
               CaselessKeyword('from') + Word(alphas + nums + '_')('table') + \
-              Optional(CaselessKeyword('where') + Combine(Word(alphas) + Word(nums) + Word(alphas)))('condition')
+              Optional(CaselessKeyword('where') + Group(delimitedList(Combine(Word(alphas) + Optional(Word(nums))) + \
+                oneOf('= != < > <= >=') + (Combine(Word(alphas) + Optional(Word(nums))) | Word(nums)))))('conditions')
 
-insert_stmt = CaselessKeyword('insert') + CaselessKeyword('into') + Word(alphas + nums + '_')('table') + \
-              Group(delimitedList(Word(alphas), ','))('columns') + \
-              CaselessKeyword('values') + Group(delimitedList(Word(alphas + nums), ','))('values')
+insert_stmt = CaselessKeyword('insert into') + Word(alphas + nums + '_')('table') + \
+              CaselessKeyword('values') + Group(delimitedList(Word(alphas + nums), ','))('values_insert')
 
-update_stmt = CaselessKeyword('update') + Word(alphas + nums + '_')('table') + \
-              CaselessKeyword('set') + delimitedList(Combine(Word(alphas) + Optional(Word(nums))), ',')('assignments') + \
-              Optional(CaselessKeyword('where') + Combine(Word(alphas) + Word(nums) + Word(alphas)))('condition')
+update_stmt = CaselessKeyword('UPDATE') + Word(alphas + nums + '_')('table') + \
+              CaselessKeyword('SET') + Group(delimitedList(Combine(Word(alphas) + Optional(Word(nums))) + \
+                Word('=') + (Combine(Word(alphas) + Optional(Word(nums))) | Word(nums)), ','))('assignments') + \
+              Optional(CaselessKeyword('where') + Group(delimitedList(Combine(Word(alphas) + Optional(Word(nums))) + \
+                oneOf('= != < > <= >=') + (Combine(Word(alphas) + Optional(Word(nums))) | Word(nums)))))('conditions')
 
 # Execute the SQL command
 def execute_sql(sql, filename):
@@ -28,38 +39,54 @@ def execute_sql(sql, filename):
         data = pd.read_csv(filename)
         if sql.startswith('SELECT'):
             # Filter data based on condition (if provided)
-            if parsed_sql.condition:
-                data = data.query(parsed_sql.condition)
+            if parsed_sql.conditions:
+                condition_elements = parsed_sql.conditions[1]
+                condition_string = ''.join(condition_elements)
+                data = data.query(condition_string)
             # Select columns from data
-            if "*" in parsed_sql.columns:
-                selected_data = data[data.columns]
-            else:
-                selected_data = data[parsed_sql.columns]
+            selected_data = data[parsed_sql.columns]
             # Return results as Pandas DataFrame
             return selected_data
-
         elif sql.startswith('INSERT'):
+            columns_names = list(data.columns)
             # Insert new row into data
-            new_row = {col: val for col, val in zip(parsed_sql.columns, parsed_sql.values)}
-            data = data.append(new_row, ignore_index=True)
+            new_rows = {col: int(val) for col, val in list(zip(columns_names, parsed_sql.values_insert))}
+            # print(new_rows)
+            data = data.append(new_rows, ignore_index=True)
             # Write updated data to CSV file
             data.to_csv(filename, index=False)
             return f'Inserted new row into {parsed_sql.table} table.'
         else:
             # Update rows in data
-            data.loc[data.eval(parsed_sql.condition), parsed_sql.assignments] = parsed_sql.values.asList()
-            # Write updated data to CSV file
-            data.to_csv(filename, index=False)
+            assignments = {}
+            for assignment in parsed_sql.assignments:
+                print(assignment)
+            #     column, value = assignment.split('=')
+            #     try:
+            #         value = int(value)
+            #     except ValueError:
+            #         pass
+            #     assignments[column.strip()] = value.strip()
+            # if parsed_sql.conditions:
+            #     condition_str = ' '.join(parsed_sql.conditions)
+            #     data.loc[data.eval(condition_str), list(assignments.keys())] = list(assignments.values())
+            # else:
+            #     data[list(assignments.keys())] = list(assignments.values())
+            # # Write updated data to CSV file
+            # data.to_csv(filename, index=False)
             return f'Updated rows in {parsed_sql.table} table.'
+
     except ParseException as e:
         print(f'Error parsing SQL: {e}')
 
+
 # Example usage
-# results = execute_sql('INSERT INTO people (name, age, gender) VALUES (John, 35, M)', 'people.csv')
+# results = execute_sql('INSERT INTO table VALUES 1,1,1,1,1,1,1,1,1,1,1,1', 'data/data1.csv')
 # print(results)
 
-results = execute_sql('SELECT ph FROM data', 'data/data.csv')
+results = execute_sql('SELECT ph FROM data', 'data/data1.csv')
 print(results)
 
-# results = execute_sql('UPDATE people SET age=36 WHERE name=John', 'people.csv')
+#
+# results = execute_sql('UPDATE data SET ph=36, Hardness=1', 'data/data.csv')
 # print(results)
